32a1b0a78ad69d8531d1c8e2f74bc4ec70246b7e
Ian Rioba <71058734+Rioba-Ian@users.noreply.github.com>
2021-01-03 11:01:15 +0300

    Update C.6.2. - Programming assignment 2.py
    on line 67, for the json.loads(str(data)), remember to remove str, let it be json.loads(data).
    infact I have replaced the whole code. It works fine

--- Unit 6 - JSON and the REST architecture/C.6.2. - Programming assignment 2.py
+++ Unit 6 - JSON and the REST architecture/C.6.2. - Programming assignment 2.py
@@ -41,30 +41,29 @@ place_id may not match for this assignment.
 """
 
 import json
-import urllib
+import urllib, urllib.parse, urllib.request
 
-#Stroring the given parameters
+#Storing the given parameters
 serviceurl = "http://python-data.dr-chuck.net/geojson?"
-sample_address = "South Federal University"
+sample_address = "South federal University"
 data_address = "Columbia University"
 address_wanted = data_address
 
 #Setting the GET parameters on the URL
-parameters = {"sensor": "false", "address": address_wanted}
-paramsurl = urllib.urlencode(parameters)
+parameters = {'sensor': 'false', 'address':address_wanted}
+paramsurl = urllib.parse.urlencode(parameters)
 
-#Generating the complete URL. Printing it in order to check if it's correct.
+#generating the complete URL.Printing it in order to check if it's correct.
 queryurl = serviceurl + paramsurl
-print("DATA URL: ", queryurl)
+print('DATA URL: ', queryurl)
 
-#Obtaining and reading the data
-data = urllib.urlopen(queryurl).read()
+data = urllib.request.urlopen(queryurl).read()
 
-#Parsing the data and looking for the field we want.
-#That field is inside the "Results" array, in its first item (if our address is 
-#correct we can assume that the result would be the correct one) and on its 
+#Parsing the data and looking for the field we wanr
+#That field is inside the "Results" array, in its forst item (if our address is 
+# correct we can assume that the result would be the correct one) and on its 
 #"place_id" field
-jsondata = json.loads(str(data))
+jsondata = json.loads(data)
 place_id = jsondata["results"][0]["place_id"]
-print("PLACE ID: ", place_id)
+print("PLACE_ID: ", place_id)
 
